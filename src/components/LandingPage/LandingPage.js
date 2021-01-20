9db1ed2bc71499cbf6ea8eb9aa44bd6285747fd6
rky9 <rajesh.coer@gmail.com>
2021-01-20 08:19:02 +0530

    removing file from src

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                Landing page....
+            </React.Fragment>
+        )
+    }
+}
