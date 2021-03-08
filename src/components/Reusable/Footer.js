df626b57d839b2973be9e541cabdc04fcc4d5fcf
rky9 <rajesh.coer@gmail.com>
2021-03-09 01:54:40 +0530

    file

---
+++ src/components/Reusable/Footer.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-block">
+        <div className="container-flude">
+          <div className="row">
+            <div className="col-12 pt-2 mx-auto text-center bg-dark">
+              <h6 className="text-white">Copy footer</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
